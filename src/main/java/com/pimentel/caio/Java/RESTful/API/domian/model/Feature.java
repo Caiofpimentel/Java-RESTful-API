e082d999385251766a7be625d5eb43e94c9436d7
Caio <caio-pimentel@hotmail.com>
2024-10-22 14:52:45 -0300

    Implementação da camada de dominio

---
+++ src/main/java/com/pimentel/caio/Java/RESTful/API/domian/model/Feature.java
@@ -0,0 +1,8 @@
+package com.pimentel.caio.Java.RESTful.API.domian.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/com/pimentel/caio/Java/RESTful/API/domian/model/News.java
@@ -0,0 +1,8 @@
+package com.pimentel.caio.Java.RESTful.API.domian.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
