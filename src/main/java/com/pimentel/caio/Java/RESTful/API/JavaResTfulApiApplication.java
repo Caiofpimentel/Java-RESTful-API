e50585bbba42402f75c3959ac70cdf927d31eb12
Caio <caio-pimentel@hotmail.com>
2024-10-22 13:41:46 -0300

    Initial commit

---
+++ src/main/java/com/pimentel/caio/Java/RESTful/API/JavaResTfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.pimentel.caio.Java.RESTful.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaResTfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pimentel/caio/Java/RESTful/API/JavaResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pimentel.caio.Java.RESTful.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
